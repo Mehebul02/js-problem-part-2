e6b4985b4738a323b549e2718888294e28d6f4aa
Mehebul02 <mehebul2123@gmail.com>
2024-02-07 17:08:35 +0600

    price addet

---
+++ price_cal.js
@@ -0,0 +1,19 @@
+function priceShopping(shirtQuantity,pantQuantity,shoeQuantity){
+    const shirtPrice =608;
+    const pantPrice =2500;
+    const shoePrice =3020;
+    const totalShirt =shirtPrice * shirtQuantity;
+    const totalPant= pantPrice * pantQuantity;
+    const totalShoe= shoePrice * shirtQuantity;
+    totalPrice =totalShirt + totalShirt + totalShoe;
+    return totalPrice;
+
+
+}
+const shopping =priceShopping(1,0,0);
+console.log(shopping);
+
+function studyTour(bus,breckfirst,dupureKhabar,ratreKhabar){
+    const busVara =50000;
+    
+}
